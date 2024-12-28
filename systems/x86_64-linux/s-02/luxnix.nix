0e5e6b9484e4f528ba89c72caa5ffb60e9a84085
maddonix <tlux14@googlemail.com>
2024-12-28 15:14:34 +0100

    fix

--- systems/x86_64-linux/s-02/luxnix.nix
+++ systems/x86_64-linux/s-02/luxnix.nix
@@ -1,11 +1,20 @@
 {config, pkgs, ...}@inputs:
 
 {
-  generic-settings = {
-    linux = {
-      # systemConfigurationPath = "${luxnixDirectory}/nixos-configurations
+  luxnix = {
+    traefik-host.enable = true;
+
+    nvidia-prime = {
+      enable = false; # enables common and desktop (with addon plasma) roles
+    };
+
+    generic-settings = {
+      enable = true;
+      hostPlatform = "x86_64-linux"; # is default
+
+      systemConfigurationPath = "/home/admin/luxnix";
       # sensitiveServiceGroupName = "sensitive-service-group"; # is default
-      # hostPlatform = "x86_64-linux"; # is default
+
       # sensitiveServiceGID = 901; # is default
 
       # Default
@@ -28,21 +37,27 @@
       # sslCertificateKeyPath = "${systemConfigurationPath}/ssl/cert.key";
       # sslCertificatePath = "${systemConfigurationPath}/ssl/cert.pem";
 
-      cpuMicrocode = "intel"; # default is "intel"
-      # processorType = "x86_64"; # default
-      # kernelPackages = pkgs.linuxPackages_latest; # default
-      kernelModules = [ "kvm-intel" ];
-      extraModulePackages = []; # default
-      # supportedFilesystems = ["btrfs"]; # default
-      # resumeDevice = "/dev/disk/by-label/nixos"; # default
-      # kernelParams = []; # default
-      # blacklistedKernelModules = []; # default
-      initrd = {
-        # supportedFilesystems = ["nfs"]; # default
-        # kernelModules = ["nfs"]; # default
-        availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+        
+      linux = {
+        cpuMicrocode = "intel"; # default is "intel"
+        # processorType = "x86_64"; # default
+        kernelPackages = pkgs.linuxPackages_latest; # default
+        kernelModules = [ "kvm-intel" ];
+        extraModulePackages = []; # default
+        initrd = {
+          kernelModules = ["dm-snapshot" ]; # default
+          availableKernelModules =  [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+        };
+
+        supportedFilesystems = ["btrfs"]; # default
+        resumeDevice = "/dev/disk/by-label/nixos"; # default
+        kernelParams = []; # default
+        
+        blacklistedKernelModules = []; # default
+
+        
+    
       };
-  
     };
   };
 }
